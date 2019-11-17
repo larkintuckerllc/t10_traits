4e3a6db822d808f612acab009d886398f454dd70
John Tucker <jtucker@appirio.com>
2019-11-17 17:37:04 -0500

    done

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,44 @@
 /* eslint-disable no-console */
 /* eslint-disable max-classes-per-file */
-interface Tr {
+/* eslint-disable class-methods-use-this */
+// eslint-disable-next-line
+type Constructable = new (...args: any[]) => object;
+
+interface TrA {
   toString: () => string;
 }
 
-class St0TrA implements Tr {
+interface TrB {
+  toAnotherString: () => string;
+}
+
+interface TrC {
+  toThirdString: () => string;
+}
+
+interface TrD {
+  toFourthString: () => string;
+}
+
+// eslint-disable-next-line
+const TrCMixin = <BC extends Constructable>(Base: BC) => {
+  return class extends Base {
+    toThirdString(): string {
+      return 'TrC - Default';
+    }
+  };
+};
+
+// eslint-disable-next-line
+const TrDMixin = <BC extends Constructable>(Base: BC) => {
+  return class extends Base {
+    toFourthString(): string {
+      return 'TrD - Default';
+    }
+  };
+};
+
+class St0 implements TrA, TrB, TrC, TrD {
   title: string;
 
   constructor(title: string) {
@@ -14,21 +48,21 @@ class St0TrA implements Tr {
   toString(): string {
     return `TrA for St0 - Title: ${this.title}`;
   }
-}
 
-class St0TrB implements Tr {
-  title: string;
-
-  constructor(title: string) {
-    this.title = title;
-  }
-
-  toString(): string {
+  toAnotherString(): string {
     return `TrB for St0 - Title: ${this.title}`;
   }
+
+  toThirdString(): string {
+    return `TrC for St0 - Title: ${this.title}`;
+  }
+
+  toFourthString(): string {
+    return `TrD for St0 - Title: ${this.title}`;
+  }
 }
 
-class St1TrA implements Tr {
+class St1 implements TrA, TrB {
   body: string;
 
   title: string;
@@ -41,32 +75,54 @@ class St1TrA implements Tr {
   toString(): string {
     return `TrA for St1 - Body: ${this.body} - Title: ${this.title}`;
   }
-}
 
-class St1TrB implements Tr {
-  body: string;
-
-  title: string;
-
-  constructor(body: string, title: string) {
-    this.body = body;
-    this.title = title;
-  }
-
-  toString(): string {
+  toAnotherString(): string {
     return `TrB for St1 - Body: ${this.body} - Title: ${this.title}`;
   }
 }
 
-const printTr = (item: Tr): void => {
-  console.log(item.toString());
+const logA = (item: TrA): void => {
+  const output = item.toString();
+  console.log(output);
 };
 
-const st0TrA = new St0TrA('T');
-const st0TrB = new St0TrB('T');
-const st1TrA = new St1TrA('B', 'T');
-const st1TrB = new St1TrB('B', 'T');
-printTr(st0TrA); // TrA for St0 - Title: T
-printTr(st0TrB); // TrB for St0 - Title: T
-printTr(st1TrA); // TrA for St1 - Body: B - Title: T
-printTr(st1TrB); // TrB for St1 - Body: B - Title: T
+const logB = (item: TrB): void => {
+  const output = item.toAnotherString();
+  console.log(output);
+};
+
+const logC = (item: TrC): void => {
+  const output = item.toThirdString();
+  console.log(output);
+};
+
+const logD = (item: TrD): void => {
+  const output = item.toFourthString();
+  console.log(output);
+};
+
+const logAB = (item: TrA & TrB): void => {
+  const output = item.toString();
+  console.log(output);
+  const output2 = item.toAnotherString();
+  console.log(output2);
+};
+
+const returnsTrA = (): TrA => new St0('T');
+const returnsTrAFlex = (option: boolean): TrA => (option ? new St0('T') : new St1('T', 'B'));
+
+const st0 = new St0('T');
+const st1 = new (TrDMixin(TrCMixin(St1)))('T', 'B');
+logA(st0); // TrA for St0 - Title: T
+logB(st0); // TrB for St0 - Title: T
+logC(st0); // TrC for St0 - Title: T
+logD(st0); // TrD for St0 - Title: T
+logA(st1); // TrA for St1 - Body: T - Title: B
+logB(st1); // TrB for St1 - Body: T - Title: B
+logC(st1); // TrC - Default
+logD(st1); // TrD - Default
+logAB(st0); // TrA for St0 - Title: T TrB for St0 - Title: T
+const item = returnsTrA();
+logA(item); // TrA for St0 - Title: T
+const item2 = returnsTrAFlex(false);
+logA(item2); // TrA for St1 - Body: T - Title: B
